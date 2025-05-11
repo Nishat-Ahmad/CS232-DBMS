340fe2cb82f97e066a514bd6c77634901b3265d8
J0hnBloodborne <m.rohaan.mirza@gmail.com>
2025-05-11 14:36:43 +0500

    no need for dotenv, database credentials now stored in .ini file and loaded using config parser

--- database/models.py
+++ database/models.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine, Column, Integer, String, Date, Numeric, Fo
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 import os
-from dotenv import load_dotenv
+import configparser
 
-load_dotenv()
-
-DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+config = configparser.ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), '..', 'db_config.ini'))
+db_cfg = config['postgresql']
+DATABASE_URL = f"postgresql://{db_cfg['user']}:{db_cfg['password']}@{db_cfg['host']}:{db_cfg['port']}/{db_cfg['database']}"
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 session = Session()
